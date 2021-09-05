106422033be6a61f255891a58b9063fc58ab7253
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2021-09-05 15:27:20 +0900

    仕入インポート処理実装 #10

--- src/fm-client/abilities/accounts-payable/purchasing/PurchasingAsBatch.js
+++ src/fm-client/abilities/accounts-payable/purchasing/PurchasingAsBatch.js
@@ -1,4 +1,4 @@
-const AbstractSinglePage = require('../../../components/AbstractSinglePage')
+const FileUploadablePage = require('../../../components/FileUploadablePage');
 const MenuItem = require('../../../components/MenuItem')
 
 const CREATE_BUTTON = 2
@@ -7,17 +7,13 @@ const MENU_ITEM = new MenuItem(5, 1, 3)
 /*
  * /仕入買掛:仕入/仕入インポート/ 
  */
-module.exports = class PurchasingAsBatch extends AbstractSinglePage {
+module.exports = class PurchasingAsBatch extends FileUploadablePage {
   constructor(page) {
     super(page)
   }
 
   async enable() {
     await super.clickOnMenu(MENU_ITEM, CREATE_BUTTON)
-    return true
-  }
-
-  create(options) {
     return true
   }
 }
--- test/fm-client/accounts-payable/purchasing/PurchasingAsBatch.spec.js
+++ test/fm-client/accounts-payable/purchasing/PurchasingAsBatch.spec.js
@@ -1,6 +1,11 @@
 const {FmClient, PurchasingAsBatch} = require('../../../../');
+const {promisify} = require('util');
+const fs = require('fs');
+const writeFileAsync = promisify(fs.writeFile);
+const unlinkAsync = promisify(fs.unlink);
 
 describe('PurchasingAsBatch', function () {
+  const filename = 'FMWW仕入.csv'
   const c = new FmClient()
 
   before(async function() {
@@ -13,10 +18,18 @@ describe('PurchasingAsBatch', function () {
 
   after(async function() {
     await c.quit()
+    await unlinkAsync(filename);
   })
 
   it.only('should import csv file.', async function () {
-    const response = await c.create()
-		expect(response).be.true
+  });
+
+  it('should fail to import csv file.', async function () {
+    await writeFileAsync(filename, ',,9999A,2099-12-31,41,1,001,10,,9900,45801703733,1,,,,9998')
+    const response = await c.create({
+			filePath: filename
+    })
+    expect(response.statusText).to.match(/^不適当なSKUがあります。.*/);
+		expect(response.isSuccess).be.false
   });
 })
