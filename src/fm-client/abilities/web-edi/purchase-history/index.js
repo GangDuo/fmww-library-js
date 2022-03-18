70bbc82422144f9b9b7961a4f383c750cf76f3ab
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2022-03-18 15:30:47 +0900

    仕入請求出力の照会ページを開く#15

--- src/fm-client/abilities/web-edi/purchase-history/index.js
+++ src/fm-client/abilities/web-edi/purchase-history/index.js
@@ -1,5 +1,9 @@
 const debug = require('../../../../diagnostics/debug')
 const AbstractSinglePage = require('../../../components/AbstractSinglePage')
+const MenuItem = require('../../../components/MenuItem')
+
+const INDEX_BUTTON = 2
+const MENU_ITEM = new MenuItem(15, 1, 4)
 
 /*
  * /WEB-EDI:WEB-EDI/仕入請求出力/
@@ -15,6 +19,17 @@ module.exports = class PurchaseHistory extends AbstractSinglePage {
   }
 
   async search(options) {
+    try {
+      await super.clickOnMenu(MENU_ITEM, INDEX_BUTTON).catch(error => {
+        throw error
+      })
+    } catch(e) {
+      return {
+        isSuccess: false,
+        statusText: e.message//'原因不明のエラー',
+      }
+    }
+
     return Promise.resolve(true)
   }
 }
